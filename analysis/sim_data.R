4138257668f13d3f46b9d5d568b0086c7e5d309f
friendlyHobbit <66422335+friendlyHobbit@users.noreply.github.com>
2024-10-29 12:20:00 +0100

    check out accidental unique states

--- analysis/sim_data.R
+++ analysis/sim_data.R
@@ -8,28 +8,42 @@ library(dplyr)
 ######### import data ###########
 
 # location of files 
-data_dir <- "C:\\Git\\SV_MasterThesis\\data"
+data_dir <- "C:\\Git\\SV_MasterThesis\\analysis\\dynamic_unknown_simoutput"
 
-json_data <- fromJSON(file.path(data_dir, "data-real.json"), flatten = TRUE)
+# get list of files in folder
+files <- list.files(data_dir, full.names = TRUE)
 
-# subset data per phase
-trainingA_df <- json_data[[1]]
-trainingB_df <- json_data[[2]]
-performanceA <- json_data[[3]]
-performanceB <- json_data[[4]]
+# df to put data in
+data_df <- data.frame()
 
-# only take dynamic
-performanceA_df <- performanceA %>% filter(isDynamic == TRUE)
-performanceB_df <- performanceB %>% filter(isDynamic == TRUE)
+# import files
+for(f in files){
+  f_data <- fromJSON(f, flatten = TRUE)
+  frames_df <- f_data[['frames']]
+  
+  # put all data in df, except frames
+  f_data[['frames']] <- NULL
+  temp_df1 <-  as.data.frame(do.call(cbind, f_data))
+  
+  # bind temp_df and frames_df
+  merged_df <- data.frame()
+  
+  for(i in rownames(frames_df)){
+    temp_df2 <- merge(frames_df[i,], temp_df1)
+    merged_df <- rbind(merged_df, temp_df2)
+  }
+  # bind all data in global df
+  data_df <- rbind(data_df, merged_df)
+}
 
 
-# unpack lists
-testdf <- performanceB_df$sessionData[[1]]
 
-testdf
+############## check for accidental unique states #############################
 
+data_df$UniqueStatePresent <- FALSE
 
-
-
-performanceA_df$sessionData[3]
+# loop through data_df
+for(i in rownames(data_df)){
+  print(data_df[i, "state"])
+}
 
